1afac669726cd7750a6828f39368132a73c46d36
Isabella <isabellamoreno1306@gmail.com>
2022-02-28 07:55:18 -0500

    taller 1

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log ("hola mundo");
+// comentarios en linea
+/*comentario 
+de 
+varias 
+lineas*/ 
+
+
+ var nombreUsuario;
+ let edadUsuario;
+// declarar variables
+var nombreVariable;
+// crear variable sin asignar valor
+/*inicializar variables:
+asignar un valor al momento de crear la variable
+*/
+var gradoEstudiante="10A";
+/*asignar un valor:
+igualar una variable existente a un nuevo valor*/
+edadUsuario=16;
+// asignar valor desde otra variable 
+console.log(edadUsuario);
+var edad=17;
+edadUsuario=edad;
+console.log(edadUsuario);
+/*asignar valor desde el teclado:
+existen dos maneras de hacerlo la primera es por ventana emergente y la segunda es por medio de un formulario en html
+*/
+var valor= prompt ("ingrese un valor");
+console.log ("la variable vale "+valor); 
+/*constantes:
+son valores que no pueden ser cambiados durante la ejecución del codigo. las constantes siempre deben ser inicializadas
+*/
+const PI= 3.1416;
+/*imprimir por consola*/
+//simple 
+console.log ("");
+console.log (valor);
+//concatenado 
---
+++ taller 1.js
@@ -0,0 +1,12 @@
+//Archivo de practica y pruebas de JavaScript.
+/*Taller 1
+fecha 28/02/2022
+realizado por: Isabella Moreno- Maria Jose Tangarife*/
+
+var nombre_de_estudiante //String
+var gradoEstudiante //double 
+var edad_de_estudiante //double 
+var valor=promt ("ingrese su edad:");
+nombre_de_estudiante= "Isabella Moreno- Maria Jose Tangarife";
+gradoEstudiante= "10A";
+nsole.log ("nombre de el estudiante" + nombre_de_estudiante +"del grado" + gradoEstudiante + "tiene" + edad_de_estudiante + "años")
