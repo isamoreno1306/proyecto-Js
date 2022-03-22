dff4e9f74f882ca0297bd1263c1463e9bdc04446
Isabella <isabellamoreno1306@gmail.com>
2022-03-22 13:31:25 -0500

    repaso.js

---
+++ repaso.js
@@ -0,0 +1,32 @@
+//primer punto 
+
+//crear variables 
+var metros;
+var kilometros;
+
+//solicitar valor al usuario 
+metros= prompt ("ingrese por favor un valor en metros");
+
+//operacion
+kilometros = metros / 1000; 
+//mostrar mensaje 
+console.log("la equivalencia es" + kilometros + "km")
+
+//segundo punto 
+
+//variables
+var distancia; 
+var litrosGastados;
+distancia = prompt ("ingrese por favor la distancia recorrida");
+
+litrosGastados = distancia * 290 / 100
+console.log ("la equivalencia de " + litrosGastados + "L")
+
+//tercer punto 
+
+//variables 
+var kilolitros;
+
+kilolitros  = litrosGastados /1000
+console.log ("la equivalencia es " + kilolitros + "kl")
+
