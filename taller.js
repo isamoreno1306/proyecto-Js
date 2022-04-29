3d764582caa58ae30e01f0f946070ccb6225a84f
Isabella <isabellamoreno1306@gmail.com>
2022-04-29 11:30:13 -0500

    taller3

---
+++ taller.js
@@ -0,0 +1,117 @@
+//TALLER 
+
+//punto 1-4
+var saludar;
+ 
+if(saludar=="si"){
+console.log("Hola, soy un condicional simple");
+}
+
+var x=20;
+
+if(x<=20){
+ console.log("la condicion es verdadera");
+}
+
+var edad;
+
+if(edad>=18){
+console.log("bienvenido, puedes ingresar");
+} else{
+console.log("lo siento, no tiene edad suficiente para entrar aqui");
+} 
+var numero1=5; 
+var numero2=8;
+if(numero1<numero2){
+console.log("numero1 no es mayor que numero2"); 
+}
+if(numero2>0){
+ console.log("numero2 es positivo");
+}
+if(numero1<0 || numero1!= 0){
+console.log("numero1 es negativo o distinto de cero");
+}
+if (numero1 >= numero2){
+console.log("incrementar en 1 unidad el valor numero1 no lo hace mayor o igual que numero2");
+}
+
+//punto 5
+var numerito = 120;
+ 
+if(numerito<50) {
+console.log("numerito sí es estrictamente menor que 50");
+}else if(numerito>=50 && numerito<=80){
+console.log("numerito sí está entre 50 y 80");
+}else if(numerito>80 && numerito<100){
+console.log("numerito es mayor que 80 pero no mayor ni igual a 100");
+}else if(numerito==100){
+console.log("numerito es igual a 100");
+}else{
+console.log("numerito es estrictamente mayor a 100");
+}
+
+//punto 6
+
+numerito=90
+
+//punto 7
+
+numerito=70
+
+//punto 8 
+
+numerito=110
+
+//punto 9 
+
+var diaActual="jueves";
+if (diaActual=="jueves"){
+console.log ("vaya al poli");
+}else{ 
+console.log ("no puede ir al poli");
+}
+
+//punto 10
+
+var valor1=2
+var valor2=4
+
+if(valor2==0){
+console.log ("no se puede dividir")
+}else{
+console.log (valor1/valor2);
+}
+
+//punto 11
+
+var dia 
+var plato 
+
+if (dia == "lunes"){
+plato ="arroz con plato";
+}else if (dia =="martes"){
+plato = "chuleta de cerdo/pollo";
+}else if (dia == "miercoles"){
+plato= "frijoles"
+
+}else if (dia== "jueves"){
+plato = "sancocho de pescado";
+}else if (dia == "viernes"){
+plato="bandeja paisa";
+}else if (dia == "sabado"){
+plato= "ajiaco";
+}else if (dia == "domingo"){
+plato ="sancocho de gallina";
+}
+console.log ("el plato del dia es ", plato);
+
+
+//punto 12
+
+var letra= "a";
+
+if (letra == "a"|| letra == "e" || letra== "i" || letra == "o" || letra== "u"){
+console.log ("es una vocal");
+}else if (letra == "0" || letra== "1" || letra == "2" || letra== "3" || letra== "4" || letra == "5" || letra =="6" || letra=="7" || letra== "8" || letra=="9"){
+console.log ("son numeros"); 
+}
