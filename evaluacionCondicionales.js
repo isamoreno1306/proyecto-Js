e2ba1476738495db0963e0123f0bfd792c7ff9da
Isabella <isabellamoreno1306@gmail.com>
2022-05-03 13:41:38 -0500

    evaluacionCondicionales.js

---
+++ evaluacionCondicionales.js
@@ -0,0 +1,57 @@
+//TALLER
+
+//Punto 1 
+
+var ValorVariable= 200;
+ 
+if (ValorVariable <=400){
+   console.log("La condición es verdadera");
+}
+
+//punto 2
+
+var valor=10;
+ 
+if (valor >= 60 || valor <= 10){
+   console.log("El valor es correcto");
+}
+
+//punto 3
+
+var numero;
+ 
+if (numero=="20"){
+console.log('El número es igual que "20" en valor y tipo');
+}else if(numero>30 && numero<40){
+console.log('El número es mayor que 30 y menor a 40');
+}else if(numero==30 || numero<=29){
+console.log("El número es igual que 30 o menor igual que 29");
+}else if(numero==40){
+console.log("El número es mayor igual que 40");
+}
+
+
+//Respuesta 3.A
+numero="20"
+//Respuesta 3.B
+numero=35
+//Respuesta 3.C
+numero=30
+//Respuesta 3.D
+numero=50
+
+//punto 4 
+
+var numerO=10;
+var numero=15;
+
+if(numerO==numero){
+console.log ("son iguales");
+}else{
+console.log (numerO+numero);
+}
+
+//punto 5 
+
+var sandwich
+
